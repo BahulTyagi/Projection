63fda709b0d5f4f46f1d83b104dddced686c77ba
BahulTyagi <bahul.tyagi20041999@gmail.com>
2022-07-09 23:06:20 +0530

    user is getting created , used express validator for validations

--- Backend/index.js
+++ Backend/index.js
@@ -4,11 +4,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())  // a middleware to see req body in conosle
-                        // console.log(req.body)
+app.use(express.json())  // a middleware to see req.body in conosle
+                         // console.log(req.body)
 
 // saare routes yaha hai
 app.use('/api/auth', require('./routes/auth'))
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,16 +1,31 @@
 const express=require('express')
 const router=express.Router()
 
+const { body, validationResult } = require('express-validator');
 const Student=require('../models/Student')
 
-
-
 // create a student using: POST "/api/auth". doesnt require login
-router.post('/', (req, res) => {
-  console.log(req.body)
-  const student=Student(req.body)
-  student.save()
-  res.send("hello")
+router.post('/', 
+[
+  body('password').isLength({ min: 5 }),
+  body('email').isEmail(),
+  body('name').isLength({min: 3})
+], 
+(req, res) => {
+  const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    Student.create({
+      id: req.body.id,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    }).then(student=>res.send(student)).catch(err=>{
+      console.log(err);
+      res.json({error: "Please enter a valid email"})
+    })
 })
 
 module.exports=router;
